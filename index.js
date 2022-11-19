f2b006a88a4515e48f8a4ee4bd88d99fb994f9a9
dhandstg <dutra.h.andre.cursos@gmail.com>
2022-11-19 16:26:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = requrie('@actions/github');
+
+try {
+    const triggerer_name = core.getInput('name_user');
+    console.log(`Name of the triggerer: ${triggerer_name}`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`Event payload:\n${payload}`);
+}
+catch(error) {
+    //log error.message and set a failing exit code
+    core.setFailed(`action failed;\n${error.message}`);
+}
